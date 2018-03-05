5f0aadc26c10c24f128937b7fe45a8adf461b761
Benjamin Bertout <bbertout@beapi.fr>
2018-03-05 17:16:28 +0100

    finish working on responsive + add overlay for tile link

--- wp-content/themes/rozario-child/header.php
+++ wp-content/themes/rozario-child/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <meta name="theme-color" content="#ff584c" />
     <meta name="format-detection" content="telephone=no" />
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
