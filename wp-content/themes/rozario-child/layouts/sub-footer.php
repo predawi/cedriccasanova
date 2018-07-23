b646ca0d1bc71b21f9d06bbaa31b28b82c856b0f
Benjamin Bertout <bbertout@beapi.fr>
2018-07-23 18:21:39 +0200

    update htaccess + add black chips mention in footer

---
+++ wp-content/themes/rozario-child/layouts/sub-footer.php
@@ -0,0 +1,16 @@
+            <div class="sub-footer">
+                <div class="container">
+                    <div class="row copyright-text">
+                        <div class="col-sm-12 text-center">
+                            <p class="mv3 mvt0"><?php echo TT::get_mod('copyright_content'); ?></p>
+                            <div class="social-links">
+                                <?php TPL::get_social_links(); ?>
+                            </div>
+                            <br>
+                        </div>
+                        <div class="col-sm-12 text-center">
+                            Made by Black Chips
+                        </div>
+                    </div>
+                </div>
+            </div>
